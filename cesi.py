bd7de58b274021b36e93b11383420e6826869f31
zaijiang <87108286+zaijiang@users.noreply.github.com>
2023-09-06 15:39:50 +0800

    Add files via upload

---
+++ cesi.py
@@ -0,0 +1,31 @@
+import streamlit as st
+st.title("标题")
+st.header("My Website")
+st.subheader("Data Analytics")
+st.text("Simple text")
+st.write("Hello World!")
+st.markdown('Streamlit is **_really_ cool**.')
+st.latex('E=MC^2')
+st.success('Success!')
+st.warning('Warning!')
+st.error('Error!')
+st.code("""
+class MyClass:
+    def __init__(self):
+        super(MyClass, self).__init__()
+""")
+st.json({
+    "pagination": {
+        "total": 100,
+        "per_page": 20,
+        "page": 1,
+    },
+    "items": [
+        {
+            "id": 1,
+            "name": "admin",
+            "email": "admin@test.com",
+            "is_superuser": True
+        }
+    ]
+})
